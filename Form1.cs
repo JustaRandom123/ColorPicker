0c3c132e2cdf4f7fd504a783a5248499cf644028
JustaRandom123 <47950815+JustaRandom123@users.noreply.github.com>
2022-06-08 21:03:40 +0200

    Added zoom | fixed rescaling calculation

--- Form1.cs
+++ Form1.cs
@@ -20,6 +20,10 @@ namespace ColorPicker
         private Point pictureLocation;
         private bool mouseInPic = false;
         private Point latestMousePosition;
+        private Size originalSize;
+        private Point originalLocation;
+
+        private int wheelCounter = 1;   
 
         public Form1()
         {
@@ -29,15 +33,43 @@ namespace ColorPicker
             this.DragEnter += Form1_DragEnter;
             this.MouseMove += Form1_MouseMove;
             this.MouseDown += Form1_MouseClick;
-
+            this.MouseWheel += Form1_MouseWheel;
             mainFrame = this;
+        }
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (picture != null)
+            {
+                if (e.Delta > 0)
+                {
+                    if (wheelCounter < 3)
+                    {
+                        wheelCounter++;
+                        zoomInAndOut(wheelCounter, "in",e.Location);
+                    }                                 
+                }
+                else
+                {
+                    if (wheelCounter > 1)
+                    {
+                        wheelCounter--;
+                        zoomInAndOut(wheelCounter, "out", e.Location);
+                    }    
+                   // else
+                   // {
+                        //pictureLocation = originalLocation;
+                      // this.Invalidate();
+                  //  }
+                }            
+            }
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
+            if (mouseInPic == false) { return; }
             if (e.Button == MouseButtons.Left && mouseInPic == true)
             {
-                //Console.WriteLine(latestMousePosition.ToString());
                Clipboard.SetText(GetPixel(new Point(latestMousePosition.X, latestMousePosition.Y)).ToString());
                CustomBox.createBox(this, "Color", GetPixel(new Point(latestMousePosition.X, latestMousePosition.Y)).ToString(), new Point(latestMousePosition.X, latestMousePosition.Y), GetPixel(new Point(latestMousePosition.X, latestMousePosition.Y)));             
             }
@@ -46,9 +78,8 @@ namespace ColorPicker
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             if (picture != null)
-            {              
-              //  Console.WriteLine(e.Location.ToString());
-                if (e.Location.X < picture.Width && e.Location.Y > 80 && e.Location.Y < picture.Height + 80)
+            {             
+                if (e.Location.X < picture.Width + pictureLocation.X && e.Location.Y > pictureLocation.Y && e.Location.Y < picture.Height + pictureLocation.Y)
                 {
                     this.Cursor = Cursors.Cross;
                     mouseInPic = true;
@@ -63,13 +94,41 @@ namespace ColorPicker
             }
         }
 
+        private void zoomInAndOut(int factor,string direction,Point cursor)
+        {
+            using (var bitmap = new Bitmap(picture))
+            {
+                if (direction == "in")
+                {
+                    Console.WriteLine("Scrolled up " + wheelCounter);
+                    Size newSize = new Size((int)(picture.Width * factor), (int)(picture.Height * factor));
+                    Bitmap bmp = new Bitmap(picture, newSize);
+                    picture = bmp;
+                    pictureLocation.Y = (int)(cursor.Y - 1.25 * (cursor.Y - pictureLocation.Y));
+                    pictureLocation.X = (int)(cursor.X - 1.25 * (cursor.X - pictureLocation.X));
+                    this.Invalidate();
+
+                }
+                else if (direction == "out")
+                {
+                    Console.WriteLine("Scrolled down " + wheelCounter);
+                    Size newSize = new Size((int)(originalSize.Width * factor), (int)(originalSize.Height * factor));
+                    Bitmap bmp = new Bitmap(picture, newSize);
+                    picture = bmp;
+                    pictureLocation.Y = (int)(cursor.Y - 0.80 * (cursor.Y - pictureLocation.Y));
+                    pictureLocation.X = (int)(cursor.X - 0.80 * (cursor.X - pictureLocation.X));
+                    this.Invalidate();
+                }
+            }
+        }
+
 
         Color GetPixel(Point position)
         {
             using (var bitmap = new Bitmap(picture))
-            {             
-                return bitmap.GetPixel(position.X,position.Y - 80);
-            }          
+            {
+                return bitmap.GetPixel(position.X - pictureLocation.X, position.Y - pictureLocation.Y);  //- 80
+            }
         }
 
     
@@ -103,7 +162,9 @@ namespace ColorPicker
                 if (ext.Equals(".png", StringComparison.CurrentCultureIgnoreCase) || ext.Equals(".jpg", StringComparison.CurrentCultureIgnoreCase))
                 {                               
                     this.picture = Image.FromFile(files[0]);
-                    this.pictureLocation = new Point(0,80);                 
+                    this.pictureLocation = new Point(0,80);
+                    originalSize = this.picture.Size;
+                    originalLocation = this.pictureLocation;
                 }
                 else
                 {
@@ -129,6 +190,8 @@ namespace ColorPicker
                 bmpRectangle.Location = this.pictureLocation;   
               
                 e.Graphics.DrawRectangle(Pens.Red, bmpRectangle);
+
+
             }
         }
 
